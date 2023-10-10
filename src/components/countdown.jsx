f72483138a11a15f410fb60df9e3760d30441bf1
rojiloro <kelengoji79@gmail.com>
2023-10-10 11:07:48 +0700

    navbar done

--- src/components/countdown.jsx
+++ src/components/countdown.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import BRI from "../assets/bri.png";
 
-
-
 function Countdown() {
   const tanggalPernikahan = new Date("2023-10-19T00:00:00");
   const [hitungMundur, setHitungMundur] = useState({
@@ -33,41 +31,43 @@ function Countdown() {
   }, []);
   return (
     <>
-      <div className="text-center sm:text-2xl text-xl py-10 min-h-screen font-monserat">
-        <div className="font-bold">
-          <h2>Countdown</h2>
-        </div>
-        <div className="flex justify-evenly mt-10">
-          <div data-aos="zoom-in" className="bg-slate-800 text-white rounded-lg h-20 p-3 text-center">
-            <p>{hitungMundur.hari}</p>
-            <p>Hari</p>
+      <div id="countdown">
+        <div className="text-center sm:text-2xl text-xl py-10 min-h-screen font-monserat">
+          <div className="font-bold">
+            <h2>Countdown</h2>
           </div>
-          <div data-aos="zoom-in" className="rounded-lg h-20 p-3 text-center bg-slate-800 text-white">
-            <p>{hitungMundur.jam}</p>
-            <p>Jam</p>
+          <div className="flex justify-evenly mt-10">
+            <div data-aos="zoom-in" className="bg-slate-800 text-white rounded-lg h-20 p-3 text-center">
+              <p>{hitungMundur.hari}</p>
+              <p>Hari</p>
+            </div>
+            <div data-aos="zoom-in" className="rounded-lg h-20 p-3 text-center bg-slate-800 text-white">
+              <p>{hitungMundur.jam}</p>
+              <p>Jam</p>
+            </div>
+            <div data-aos="zoom-in" className="rounded-lg h-20 p-3 text-center bg-slate-800 text-white">
+              <p>{hitungMundur.menit}</p>
+              <p>Menit</p>
+            </div>
+            <div data-aos="zoom-in" className="rounded-lg h-20 p-3 text-center bg-slate-800 text-white">
+              <p>{hitungMundur.detik}</p>
+              <p>Detik</p>
+            </div>
           </div>
-          <div data-aos="zoom-in" className="rounded-lg h-20 p-3 text-center bg-slate-800 text-white">
-            <p>{hitungMundur.menit}</p>
-            <p>Menit</p>
-          </div>
-          <div data-aos="zoom-in" className="rounded-lg h-20 p-3 text-center bg-slate-800 text-white">
-            <p>{hitungMundur.detik}</p>
-            <p>Detik</p>
-          </div>
-        </div>
-        <div className="bg-orange-100 -mb-10 font-monserat">
-          <div className="mt-60">
-            <h1 className="font-bold">Gift</h1>
-          </div>
-          <div className="sm:flex block sm:m-0 mx-auto justify-evenly sm:p-28 p-4 ">
-            <div data-aos="fade-right" className="p-5 sm:w-[80%] w-[90%] sm:my-0 my-5 mx-auto bg-white rounded-md shadow-lg hover:shadow-xl hover:scale-110 transition flex items-center">
-              <div className="font-semibold text-start w-1/2 sm:text-base text-lg">
-                <p className="font-semibold">BRI</p>
-                <p className="font-light">Zumrotun Alfiyah</p>
-                <p>661001028119531</p>
-              </div>
-              <div className="w-1/2 flex justify-end">
-                <img src={BRI} alt="bri" className="w-[30%]" />
+          <div id="gift" className="bg-orange-100 -mb-10 font-monserat">
+            <div className="mt-60">
+              <h1 className="font-bold">Gift</h1>
+            </div>
+            <div className="sm:flex block sm:m-0 mx-auto justify-evenly sm:p-28 p-4 ">
+              <div data-aos="fade-right" className="p-5 sm:w-[80%] w-[90%] sm:my-0 my-5 mx-auto bg-white rounded-md shadow-lg hover:shadow-xl hover:scale-110 transition flex items-center">
+                <div className="font-semibold text-start w-1/2 sm:text-base text-lg">
+                  <p className="font-semibold">BRI</p>
+                  <p className="font-light">Zumrotun Alfiyah</p>
+                  <p>661001028119531</p>
+                </div>
+                <div className="w-1/2 flex justify-end">
+                  <img src={BRI} alt="bri" className="w-[30%]" />
+                </div>
               </div>
             </div>
           </div>
